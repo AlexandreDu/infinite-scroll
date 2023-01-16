6e2c825bc2c177c651fd30da56e5a94f18e5458a
Alex <alexandre.dutay@outlook.fr>
2023-01-16 14:28:09 +0100

    Avatar sizes fix

--- src/components/user/index.tsx
+++ src/components/user/index.tsx
@@ -1,14 +1,11 @@
 import type { UserProps } from '../../types';
 
 import * as Styled from './styles';
-import { useTheme } from 'styled-components';
 import { Avatar } from '../avatar';
 
 export function User({ images, name }: UserProps) {
-  const theme = useTheme();
-
   const srcSet = `${images.small} 32w, ${images.medium} 64w, ${images.large} 128w`;
-  const sizes = `(min-width: ${theme.breakpoints.lg}) 128vw, (min-width: ${theme.breakpoints.sm}) 64vw, 32vw`;
+  const sizes = `48px`;
   return (
     <Styled.User>
       <Avatar src={images.small} srcSet={srcSet} sizes={sizes} alt={name} />
