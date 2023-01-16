0c1e4b1571b0a32cfb4e7e659eab720ec06cdaa0
Alex <alexandre.dutay@outlook.fr>
2023-01-16 11:16:56 +0100

    cleaning

--- src/api/config/index.ts
+++ src/api/config/index.ts
@@ -2,13 +2,6 @@ import axios from 'axios';
 
 const ACCESS_KEY = import.meta.env.VITE_UNSPLASH_ACESS_KEY;
 
-export const axiosClient = axios.create({
-  baseURL: `https://jsonplaceholder.typicode.com/photos`,
-  headers: {
-    'Content-Type': 'application/json',
-  },
-});
-
 export const unsplashAxios = axios.create({
   baseURL: `https://api.unsplash.com/photos`,
   headers: {
